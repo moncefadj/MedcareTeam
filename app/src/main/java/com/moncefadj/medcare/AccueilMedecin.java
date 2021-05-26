ce0d44188353dd19b1608a6c6c00bf8033cc93ea
mohammed <mohammedmekdour.23@gmail.com>
2021-05-26 02:03:42 +0100

    moh's work

---
+++ app/src/main/java/com/moncefadj/medcare/AccueilMedecin.java
@@ -0,0 +1,14 @@
+package com.moncefadj.medcare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccueilMedecin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil_medecin);
+    }
+}

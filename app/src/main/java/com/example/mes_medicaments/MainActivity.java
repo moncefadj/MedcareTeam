1cd8ad678d5891652d35a2f9f9812f143a3620b1
LENONVO <as.madaniyousfi@esi-sba.dz>
2021-05-29 00:42:57 +0200

    init my branch, adding med feature

---
+++ app/src/main/java/com/example/mes_medicaments/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.mes_medicaments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mes_medicaments.adapter.medAdapter;
+import com.example.mes_medicaments.models.medData;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TIME3 = "TIME3" ;
+    private static final int ADD_MED_ACTIVITY = 0;
+
+    public static final String NAME = "NAME";
+    public static final String DESCR = "DESCRIPTION";
+    public static final String TIME = "TIME";
+    public static final String TIME2 = "TIME2";
+    private FloatingActionButton ajouter_med;
+    private TextView nom , description , temps;
+    private RecyclerView malist ;
+    private MainViewModel viewModel;
+    private medAdapter adapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewModel = new ViewModelProvider(this).get(MainViewModel.class);
+        adapter =new medAdapter(this,viewModel);
+        malist = (RecyclerView) findViewById(R.id.malist);
+        malist.setLayoutManager(new LinearLayoutManager(this));
+        malist.setAdapter(adapter);
+        ajouter_med = (FloatingActionButton) findViewById(R.id.add_med);
+        ajouter_med.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openajouterMed();
+            }
+        });
+
+    }
+    private void openajouterMed(){
+        Intent intent = new Intent(this , ajouterMed.class);
+        startActivityForResult(intent, ADD_MED_ACTIVITY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == ADD_MED_ACTIVITY) {
+            if (resultCode == RESULT_OK) {
+                String name = data.getStringExtra(NAME);
+                String desc = data.getStringExtra(DESCR);
+                String time = data.getStringExtra(TIME);
+                String time2 = data.getStringExtra(TIME2);
+                String time3 = data.getStringExtra(TIME3);
+
+                viewModel.addMed(new medData(name,desc,time,time2,time3,R.drawable.medicine));
+                adapter.notifyDataSetChanged();
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/mes_medicaments/ajouterMed.java
@@ -0,0 +1,165 @@
+package com.example.mes_medicaments;
+
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
+
+
+public class ajouterMed extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener, AdapterView.OnItemClickListener , DatePickerDialog.OnDateSetListener {
+    private EditText EnomMed;
+    private EditText Description;
+    private EditText datedebut, datefin ;
+    private TextView heure , heure2,heure3;
+    private Button ajouter_med ;
+    private Button ajouter_au_list;
+    private  int index =0;
+    private Spinner instructions ;
+    DatePickerDialog datePickerDialog;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter);
+        EnomMed = (EditText) findViewById(R.id.nomMed);
+        heure = (TextView) findViewById(R.id.heure);
+        heure2 = (TextView) findViewById(R.id.heure2);
+        heure3 = (TextView) findViewById(R.id.heure3);
+        Description = (EditText) findViewById(R.id.description);
+        ajouter_med = (Button) findViewById(R.id.ajoutertemps);
+        ajouter_au_list = (Button ) findViewById(R.id.ajouterAuliste);
+        instructions = (Spinner) findViewById(R.id.spinnerInst);
+        ArrayAdapter<CharSequence> instructAdapter = ArrayAdapter.createFromResource(this, R.array.instructions, android.R.layout.simple_spinner_item);
+        instructAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        instructions.setAdapter(instructAdapter);
+        instructions.getOnItemSelectedListener();
+        datedebut = (EditText) findViewById(R.id.dateDebut);
+        datefin = (EditText)  findViewById(R.id.dateFin);
+        ajouter_med.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opentimepicker();
+
+            }
+        });
+        ajouter_au_list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData();
+            }
+        });
+        datedebut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDatePicker();
+            }
+        });
+        datefin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               openDatePicker2();
+
+            }
+        });
+
+
+    }
+
+
+    private void sendData() {
+        String name = EnomMed.getText().toString();
+        String descrip = Description.getText().toString();
+        String time = heure.getText().toString();
+        String time2 = heure2.getText().toString();
+        String time3 = heure3.getText().toString();
+
+        Intent intent = new Intent();
+        intent.putExtra(MainActivity.NAME, name);
+        intent.putExtra(MainActivity.DESCR, descrip);
+        intent.putExtra(MainActivity.TIME, time);
+        intent.putExtra(MainActivity.TIME2 , time2);
+        intent.putExtra(MainActivity.TIME3 , time3);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    private void opentimepicker() {
+        DialogFragment timePickerDialog = new TimePickerFragment();
+        timePickerDialog.show(getSupportFragmentManager(), "time picker");
+
+    }
+    public void onTimeSet(TimePicker timePicker, int intHourOfDay, int intMinute) {
+        if(heure.getText().toString().equals("")) {
+
+            heure.setText((intHourOfDay + ":" + intMinute));
+        }
+        else if (heure2.getText().toString().equals("")){
+            heure2.setText((intHourOfDay + ":" + intMinute ));
+        }
+        else {
+            heure3.setText((intHourOfDay + ":" + intMinute ));
+        }
+
+
+        ajouter_med.setText("ajouter une autre heure de prise ");
+        ajouter_med.setTextColor(Color.GRAY);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        String choice = parent.getItemAtPosition(position).toString();
+        Toast.makeText(getApplicationContext(),choice, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        datedebut.setText(dayOfMonth +","+ month +"," + year);
+    }
+    public void openDatePicker(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog = new DatePickerDialog(ajouterMed.this,R.style.AppCompatDialogStyle ,new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                datedebut.setText(day+"/" + month + "/" + year);
+
+            }
+        },year,month,day);
+        datePickerDialog.show();
+    }
+    public void openDatePicker2(){
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog = new DatePickerDialog(ajouterMed.this,R.style.AppCompatDialogStyle ,new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                datefin.setText(day+"/" + month + "/" + year);
+
+            }
+        },year,month,day);
+        datePickerDialog.show();
+    }
+}
